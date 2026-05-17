15457eef02679c563c1fed8c3be177560461d6a6
Kyubinn Kim <dannykim2026@u.northwestern.edu>
2026-05-16 20:48:12 -0500

    feat(knight): generate 8 L-shape candidate moves from a square

--- src/main/java/domain/Knight.java
+++ src/main/java/domain/Knight.java
@@ -1,6 +1,20 @@
 package domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Knight extends Piece {
+	private static final int[][] L_SHAPE_OFFSETS = {
+		{2, 1},
+		{2, -1},
+		{-2, 1},
+		{-2, -1},
+		{1, 2},
+		{1, -2},
+		{-1, 2},
+		{-1, -2}
+	};
+
 	public Knight(Color color) {
 		super(color);
 	}
@@ -8,5 +22,13 @@ public class Knight extends Piece {
 	@Override
 	public PieceType type() {
 		return PieceType.KNIGHT;
+	}
+
+	public Set<Square> candidateMoves(Square from, Board board) {
+		Set<Square> moves = new HashSet<>();
+		for (int[] offset : L_SHAPE_OFFSETS) {
+			from.offset(offset[0], offset[1]).ifPresent(moves::add);
+		}
+		return moves;
 	}
 }
--- src/test/java/domain/KnightTest.java
+++ src/test/java/domain/KnightTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,20 @@ public class KnightTest {
 		Knight knight = new Knight(Color.WHITE);
 		Color expectedColor = Color.WHITE;
 		PieceType expectedType = PieceType.KNIGHT;
+
 		Assertions.assertEquals(expectedColor, knight.color());
 		Assertions.assertEquals(expectedType, knight.type());
+	}
+
+	@Test
+	public void candidateMovesFromCenterOfEmptyBoardReturnsEightSquares() {
+		Knight knight = new Knight(Color.WHITE);
+		Board board = new Board();
+		Square d4 = Square.of(3, 3);
+		board.place(d4, knight);
+
+		Set<Square> moves = knight.candidateMoves(d4, board);
+
+		Assertions.assertEquals(8, moves.size());
 	}
 }
