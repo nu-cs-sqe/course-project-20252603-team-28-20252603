5dcb426e3b1a71398886d6a4ef25f7f0a7bb8880
Jiahao-Deng03 <Dengjiahao43@gmail.com>
2026-05-31 11:38:26 -0500

    feat(pawn): support white single-step moves

--- src/main/java/domain/Pawn.java
+++ src/main/java/domain/Pawn.java
@@ -1,6 +1,13 @@
 package domain;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Pawn extends Piece {
+	private static final int WHITE_FORWARD = 1;
+
 	public Pawn(Color color) {
 		super(color);
 	}
@@ -8,5 +15,15 @@ public class Pawn extends Piece {
 	@Override
 	public PieceType type() {
 		return PieceType.PAWN;
+	}
+
+	@Override
+	public Set<Square> candidateMoves(Square from, Board board) {
+		Objects.requireNonNull(from);
+		Objects.requireNonNull(board);
+
+		Set<Square> moves = new HashSet<>();
+		from.offset(0, WHITE_FORWARD).ifPresent(moves::add);
+		return Collections.unmodifiableSet(moves);
 	}
 }
