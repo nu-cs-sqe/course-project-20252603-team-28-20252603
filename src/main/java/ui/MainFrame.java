f4afc50158e83012a32f861bfb3cd63881933973
Kyubinn Kim <dannykim2026@u.northwestern.edu>
2026-06-07 13:28:53 +0900

    feat(ui): MainFrame ticks clock and ends game on timeout

--- src/main/java/ui/MainFrame.java
+++ src/main/java/ui/MainFrame.java
@@ -1,9 +1,14 @@
 package ui;
 
+import java.util.Optional;
+
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 import domain.Board;
+import domain.Color;
 import domain.Game;
+import domain.GameStatus;
 import intl.Messages;
 
 // Main window for the chess game.
@@ -13,6 +18,22 @@ public class MainFrame extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setExtendedState(MAXIMIZED_BOTH);
 		add(new BoardPanel(game, board));
+
+		Timer timer = new Timer(1000, e -> {
+			if (game.getStatus() != GameStatus.IN_PROGRESS) {
+				((Timer) e.getSource()).stop();
+				return;
+			}
+			Optional<Color> winner = game.winnerByTimeout();
+			if (winner.isPresent()) {
+				Color loser = winner.get() == Color.WHITE
+					? Color.BLACK : Color.WHITE;
+				game.resign(loser);
+				((Timer) e.getSource()).stop();
+			}
+		});
+		timer.start();
+
 		setVisible(true);
 	}
 }
