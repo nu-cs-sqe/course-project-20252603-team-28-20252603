6e9be0b18b7832c2be88fdcdae3ce1687e81627d
Kyubinn Kim <dannykim2026@u.northwestern.edu>
2026-06-09 08:19:49 +0900

    test(square): not equal to a non-square object

--- src/test/java/domain/SquareTest.java
+++ src/test/java/domain/SquareTest.java
@@ -62,4 +62,9 @@ public class SquareTest {
 
 		Assertions.assertNotEquals(firstSquare, secondSquare);
 	}
+
+	@Test
+	public void squareNotEqualToNonSquareExpectFalse() {
+		Assertions.assertNotEquals(Square.of(1, 1), "not a square");
+	}
 }
