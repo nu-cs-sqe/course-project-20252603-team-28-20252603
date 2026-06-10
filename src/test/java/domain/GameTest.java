73a54faa319fee412cbb3eccbf372213774f7f8f
Kyubinn Kim <dannykim2026@u.northwestern.edu>
2026-06-11 07:30:11 +0900

    test(game): makeMove castles queenside

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -938,4 +938,44 @@ public class GameTest {
 		game.makeMove(Square.of(4, 0), Square.of(6, 0));
 		EasyMock.verify(boardMock);
 	}
+
+	@Test
+	public void whiteQueensideCastleMovesKingAndRook() {
+		Board boardMock = EasyMock.createMock(Board.class);
+		Piece whiteKing = getMockedPiece(Color.WHITE, PieceType.KING);
+		Piece whiteRook = getMockedPiece(Color.WHITE, PieceType.ROOK);
+		Piece blackKing = getMockedPiece(Color.BLACK, PieceType.KING);
+		EasyMock.expect(boardMock.pieceAt(Square.of(4, 0)))
+			.andStubReturn(Optional.of(whiteKing));
+		EasyMock.expect(boardMock.pieceAt(Square.of(0, 0)))
+			.andStubReturn(Optional.of(whiteRook));
+		EasyMock.expect(boardMock.pieceAt(Square.of(1, 0))).andStubReturn(Optional.empty());
+		EasyMock.expect(boardMock.pieceAt(Square.of(2, 0))).andStubReturn(Optional.empty());
+		EasyMock.expect(boardMock.pieceAt(Square.of(3, 0))).andStubReturn(Optional.empty());
+		EasyMock.expect(boardMock.pieceAt(Square.of(4, 7)))
+			.andStubReturn(Optional.of(blackKing));
+		EasyMock.expect(boardMock.findKing(Color.WHITE)).andStubReturn(Square.of(4, 0));
+		EasyMock.expect(boardMock.findKing(Color.BLACK)).andStubReturn(Square.of(4, 7));
+		EasyMock.expect(boardMock.occupiedSquaresOf(Color.BLACK))
+			.andStubReturn(Set.of(Square.of(4, 7)));
+		EasyMock.expect(boardMock.occupiedSquaresOf(Color.WHITE))
+			.andStubReturn(Set.of(Square.of(4, 0), Square.of(0, 0)));
+		EasyMock.expect(blackKing.candidateMoves(Square.of(4, 7), boardMock))
+			.andStubReturn(Set.of(Square.of(3, 7)));
+		EasyMock.expect(whiteKing.candidateMoves(Square.of(4, 0), boardMock))
+			.andStubReturn(Set.of());
+		EasyMock.expect(whiteRook.candidateMoves(Square.of(0, 0), boardMock))
+			.andStubReturn(Set.of());
+		EasyMock.expect(boardMock.copy()).andStubReturn(boardMock);
+		boardMock.move(Square.of(4, 0), Square.of(2, 0));
+		EasyMock.expectLastCall();
+		boardMock.move(Square.of(0, 0), Square.of(3, 0));
+		EasyMock.expectLastCall();
+		boardMock.move(EasyMock.anyObject(Square.class), EasyMock.anyObject(Square.class));
+		EasyMock.expectLastCall().anyTimes();
+		EasyMock.replay(boardMock, whiteKing, whiteRook, blackKing);
+		Game game = new Game(boardMock);
+		game.makeMove(Square.of(4, 0), Square.of(2, 0));
+		EasyMock.verify(boardMock);
+	}
 }
