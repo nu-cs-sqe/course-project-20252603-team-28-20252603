2ed8c98532632b456d44c674df55e1859c816981
Kyubinn Kim <dannykim2026@u.northwestern.edu>
2026-06-11 10:02:08 +0900

    test(game): a king that has moved blocks castling

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -1037,4 +1037,37 @@ public class GameTest {
 		game.makeMove(Square.of(7, 0), Square.of(7, 1));
 		Assertions.assertFalse(game.canCastle(Color.WHITE, CastlingSide.KINGSIDE));
 	}
+
+	@Test
+	public void cannotCastleAfterTheKingHasMoved() {
+		Board boardMock = EasyMock.createMock(Board.class);
+		Piece whiteKing = getMockedPiece(Color.WHITE, PieceType.KING);
+		Piece whiteRook = getMockedPiece(Color.WHITE, PieceType.ROOK);
+		Piece blackKing = getMockedPiece(Color.BLACK, PieceType.KING);
+		EasyMock.expect(boardMock.pieceAt(Square.of(4, 0)))
+			.andStubReturn(Optional.of(whiteKing));
+		EasyMock.expect(boardMock.pieceAt(Square.of(7, 0)))
+			.andStubReturn(Optional.of(whiteRook));
+		EasyMock.expect(boardMock.pieceAt(Square.of(4, 1)))
+			.andStubReturn(Optional.of(whiteKing));
+		EasyMock.expect(boardMock.pieceAt(Square.of(4, 7)))
+			.andStubReturn(Optional.of(blackKing));
+		EasyMock.expect(boardMock.findKing(Color.WHITE)).andStubReturn(Square.of(4, 1));
+		EasyMock.expect(boardMock.findKing(Color.BLACK)).andStubReturn(Square.of(4, 7));
+		EasyMock.expect(boardMock.occupiedSquaresOf(Color.BLACK))
+			.andStubReturn(Set.of(Square.of(4, 7)));
+		EasyMock.expect(boardMock.occupiedSquaresOf(Color.WHITE))
+			.andStubReturn(Set.of(Square.of(4, 1)));
+		EasyMock.expect(blackKing.candidateMoves(Square.of(4, 7), boardMock))
+			.andStubReturn(Set.of(Square.of(3, 7)));
+		EasyMock.expect(whiteKing.candidateMoves(Square.of(4, 1), boardMock))
+			.andStubReturn(Set.of());
+		EasyMock.expect(boardMock.copy()).andStubReturn(boardMock);
+		boardMock.move(EasyMock.anyObject(Square.class), EasyMock.anyObject(Square.class));
+		EasyMock.expectLastCall().anyTimes();
+		EasyMock.replay(boardMock, whiteKing, whiteRook, blackKing);
+		Game game = new Game(boardMock);
+		game.makeMove(Square.of(4, 0), Square.of(4, 1));
+		Assertions.assertFalse(game.canCastle(Color.WHITE, CastlingSide.KINGSIDE));
+	}
 }
