0b52e82bb9316c4b64e48f9c3211d72d9fcc0040
superalv123 <superalv123@gmail.com>
2026-06-11 19:27:06 -0500

    Fix checkstyle import error

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -2,7 +2,6 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
